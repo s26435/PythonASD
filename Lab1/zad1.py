d1db488c8d4c1e95e5105d689057d443e76c195e
s26435 <s26435@pjwstk.edu.pl>
2024-02-19 11:38:45 +0100

    Initial commit

---
+++ Lab1/zad1.py
@@ -0,0 +1,15 @@
+def czy_trojka_pitagorejska(a, b, c):
+    sorted_numbers = sorted([a, b, c])
+    if sorted_numbers[0]**2 + sorted_numbers[1]**2 == sorted_numbers[2]**2:
+        return True
+    else:
+        return False
+
+a = int(input("Podaj pierwszą liczbę: "))
+b = int(input("Podaj drugą liczbę: "))
+c = int(input("Podaj trzecią liczbę: "))
+
+if czy_trojka_pitagorejska(a, b, c):
+    print(f"{a}, {b}, {c} to trójka pitagorejska.")
+else:
+    print(f"{a}, {b}, {c} to nie trójka pitagorejska.")
