2f30919dca6edc58a01dc70ffbbd434fcfbf32d7
s26435 <s26435@pjwstk.edu.pl>
2024-02-26 12:08:01 +0100

    zadania 1-4 z lab 2

--- Lab2/zad 2.py
+++ Lab2/zad 2.py
@@ -3,18 +3,9 @@ Jan Wolski 2/26/24
 Kodowanie za pomocą klucza i xor
 """
 
-
-def text_to_ascii(text):
-    ascii = [ord(a) for a in text]
-    return ascii
-
-
-def ascii_to_text(table):
-    return ''.join(chr(num) for num in table)
-
 def encrypt(text, mask):
-    text = text_to_ascii(text)
-    return ascii_to_text([a ^ mask for a in text])
+    text = [ord(a) for a in text]
+    return ''.join([chr(a ^ mask) for a in text])
 
 
 print(encrypt("Dupa", 0b11111))
---
+++ Lab2/zad 5.py
@@ -0,0 +1,13 @@
+def binary(num):
+    if num == 0:
+        return '0'
+
+    binary_num = ''
+    while num > 0:
+        r = num % 2
+        binary_num = str(r) + binary_num
+        num //= 2
+
+    return binary_num
+
+print(binary(10))
