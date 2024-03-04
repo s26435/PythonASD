68ad828b978fcca5292f37f4b8c20a68fda97ed9
s26435 <s26435@pjwstk.edu.pl>
2024-03-04 12:04:08 +0100

    zadanie 5 z labolatorium 3

--- Lab3/zad4.py
+++ Lab3/zad4.py
@@ -1,5 +1,38 @@
 """
 zadanie 5 z kartki
 """
-from zad1 import miejsceZerowe
+from math import nan
+import sys
+def pierwiastek_liczby(a, n):
+    return pow(a, 2) - n
 
+
+
+def miejsceZerowe(funkcja, a, b, n, epsilon):
+    if a >= b:
+        print("Przediał nie może zaczynać się od wiekszej liczby niż sie kończy!", file=sys.stderr)
+        return nan
+
+    ya = funkcja(a, n)
+    yb = funkcja(b, n)
+
+    if ya*yb >= 0:
+        print("W podanym przedizale musi istnieć miejsce zerowe!", file=sys.stderr)
+        return nan
+
+    while True:
+        x = (a + b) / 2
+        y = funkcja(x, n)
+        if ya * y < 0:
+            b = x
+        else:
+            a = x
+            ya = y
+        if abs(b-a) < epsilon:
+            break
+        if y == 0:
+            break
+    return x
+
+n = 4
+print(miejsceZerowe(pierwiastek_liczby, 0, 3, n, 1e-10))
